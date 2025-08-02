6f147d12c10edbf1c46de3b692622c9e386062cb
colts18seth <colts18seth@gmail.com>
2025-08-02 11:17:39 -0400

    Added --verbose flag

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ if len(sys.argv) < 2:
     print("Error: Missing an argument!")
     sys.exit(1)
 
+verboseFlag = '--verbose' in sys.argv
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
@@ -14,6 +16,9 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=sys.argv[1]
 )
+if verboseFlag and sys.argv[2] == '--verbose':
+    print(f'User prompt: {sys.argv[1]}')
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+
 print(response.text)
-print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
